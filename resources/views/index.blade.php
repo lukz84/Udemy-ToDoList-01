cc12d805b2c55414faa207b78062c9552eb4fb59
shodan <lukzz84@gmail.com>
2025-07-13 22:39:05 +0200

    2

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,2 +1,16 @@
 <h1>Kurwa to za proste ale robie po koleji</h1>
-{{ $name }}
+
+
+<div>
+{{-- @if (count($tasks) > 0) --}}
+@forelse ($tasks as $task )
+<div>
+    <a href="{{ route('tasks.show', ['id' => $task->id]) }}">{{ $task->title }}</a>
+
+</div>
+@empty
+<div> No task Here </div>
+
+@endforelse
+
+</div>
